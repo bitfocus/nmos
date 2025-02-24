e054e86a1685592a5611c9296cc9bd13758e5a6f
Erlend Jones <erlend.jones@gmail.com>
2025-02-24 13:34:30 +0100

    deactivate v1.0 v1.1, and fix imports

--- src/schema/is04/v1.0/index.ts
+++ src/schema/is04/v1.0/index.ts
@@ -1,25 +1,5 @@
-import node from './zod/node.json'
-import devices from './zod/devices.json'
-import device from './zod/device.json'
-import senders from './zod/senders.json'
-import sender from './zod/sender.json'
-import receivers from './zod/receivers.json'
-import receiver from './zod/receiver.json'
-import sources from './zod/sources.json'
-import source from './zod/source.json'
-import flows from './zod/flows.json'
-import flow from './zod/flow.json'
+
 
 export default {
-	'/node/v1.0/self': node,
-	'/node/v1.0/devices': devices,
-	'/node/v1.0/devices/{device_id}': device,
-	'/node/v1.0/senders': senders,
-	'/node/v1.0/senders/{sender_id}': sender,
-	'/node/v1.0/receivers': receivers,
-	'/node/v1.0/receivers/{receiver_id}': receiver,
-	'/node/v1.0/sources': sources,
-	'/node/v1.0/sources/{source_id}': source,
-	'/node/v1.0/flows': flows,
-	'/node/v1.0/flows/{flow_id}': flow,
+	
 }
