b4f4dae6343acc9b809e80a71c6a2587ca685c7d
Erlend Jones <erlend.jones@gmail.com>
2025-04-30 17:55:16 +0200

    clean up json postfix even more

--- src/schema/is04/v1.3/zod/_sources.ts
+++ src/schema/is04/v1.3/zod/_sources.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { _audioChannels } from "./_propAudioChannels"
 import { _nmosResourceBase } from './_nnosResourceBase'
 import { _sourceBase } from './_sourceBase'
-import _source from './_source.json'
+import _source from './_source'
 
 export default z
 	.array(
