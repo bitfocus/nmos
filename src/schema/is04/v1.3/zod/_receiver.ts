d69cf5a189e100d5006d11ad2d16ae2afa6d3e0e
Erlend Jones <erlend.jones@gmail.com>
2025-07-07 19:08:34 +0200

    fix missing mux item

--- src/schema/is04/v1.3/zod/_receiver.ts
+++ src/schema/is04/v1.3/zod/_receiver.ts
@@ -4,8 +4,9 @@ import { _receiverAudioCore } from './_receiverAudioCore'
 import { _receiverBase } from './_receiverBase'
 import { _receiverDataCore } from './_receiverDataCore'
 import { _receiverVideoCore } from './_receiverVideoCore'
+import { _receiverMuxCore } from './_receiverMuxCore'
 
 export default _nmosResourceBase
 	.and(_receiverBase)
-	.and(z.discriminatedUnion('format', [_receiverAudioCore, _receiverVideoCore, _receiverDataCore]))
+	.and(z.discriminatedUnion('format', [_receiverAudioCore, _receiverVideoCore, _receiverDataCore, _receiverMuxCore]))
 	.describe('Describes a Receiver')
