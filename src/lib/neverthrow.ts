dc5529712ee3202f1757c0050cf31787dca34055
Erlend Jones <erlend.jones@gmail.com>
2025-07-07 17:12:04 +0200

    add neverthrow types

--- src/lib/neverthrow.ts
+++ src/lib/neverthrow.ts
@@ -7,3 +7,23 @@ export type NeverthrowError = SUPERAGENT_ERROR | ZOD_WRAPPED_ERROR
 export type NeverthrowResult<T, E = never> = [E] extends [never]
 	? { ok: T; error: null } | { ok: null; error: NeverthrowError }
 	: { ok: T; error: null } | { ok: null; error: NeverthrowError } | E
+
+export type NeverthrowZodResult<T> =
+	| {
+			ok: T
+			error: null
+	  }
+	| {
+			ok: null
+			error: ZOD_WRAPPED_ERROR
+	  }
+
+export type NeverthorwSuperagentResult<T> =
+	| {
+			ok: T
+			error: null
+	  }
+	| {
+			ok: null
+			error: SUPERAGENT_ERROR
+	  }
